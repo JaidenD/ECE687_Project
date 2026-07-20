7e638077d109c7a439459c9d0fc1b423442e28d5
lomaichi <ellieluo2003@gmail.com>
2026-07-20 16:26:41 -0400

    minor update

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
-xhost +
+xhost +local:
 
 docker run -it --rm \
-	--network=host --pid=host --ipc=host \
-	--volume ./linked_folder:/linked_folder:rw \
-	--volume "$HOME/.Xauthority:/root/.Xauthority:rw" \
-	--env="DISPLAY" \
-	--name="dji_robomaster_ros_simulator" dji_robomaster_ros:1.0 \
+    --network=host \
+    --pid=host \
+    --ipc=host \
+    --device=/dev/dri:/dev/dri \
+    -e DISPLAY=$DISPLAY \
+    -e XDG_RUNTIME_DIR=/tmp/runtime-root \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/root/.Xauthority \
+    -v ./linked_folder:/linked_folder \
+    --name dji_robomaster_ros_simulator \
+    dji_robomaster_ros:1.0\
 	/bin/bash -c "cd linked_folder/ros_ws_sim && colcon build && source install/setup.bash && ros2 run multi_robomaster_ros_sim simulator"
